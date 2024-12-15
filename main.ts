06a2b65d4390809c25efb8253c67787f5ae124cb
Kristan <itan_07@hotmail.com>
2024-12-15 10:18:12 -0600

    Added new commands to main

--- main.ts
+++ main.ts
@@ -3,36 +3,36 @@ import moment from 'moment';
 import WolfCommand from "./src/commands/testing/wolf";
 import HelpCommand from './src/commands/general/help';
 import ReceiveImgCommand from './src/commands/testing/img';
-import AddMemberCommand from './src/commands/admin/addmember';
-import DeleteAdmin from './src/commands/admin/deletemember';
+import AddMemberCommand from './src/commands/admin/members/addmember';
+import DeleteAdmin from './src/commands/admin/members/deletemember';
 import GetProfileInfoCommand from './src/commands/general/perfil';
 import TestCommand from './src/commands/testing/prueba';
 import OtherCommand from './src/commands/testing/other';
 import DuelCommand from './src/commands/general/duel';
 import DuelWinCommand from './src/commands/general/duelwin';
-import SeeMembersCommand from './src/commands/general/members';
+import VerMiembrosCommand from './src/commands/general/miembros';
+import RegistrarGrupoCommand from './src/commands/admin/groups/registrargrupo';
+import RegisteredGroupsCommand from './src/commands/admin/groups/gruposregistrados';
+
 
 //For some reason this bot stops working if a group has ( or ) in its name on production server ubuntu server!
 
-export function Main() {
-  //Set moment library to spanish
-  moment.locale("es");
+//Set moment library to spanish
+moment.locale("es");
 
-  const klBot = new Bot({ prefix: "!", coolDownSecondsTime: 1 });
-  klBot.AddCommand(new WolfCommand());
-  klBot.AddCommand(new HelpCommand());
-  klBot.AddCommand(new ReceiveImgCommand());
-  klBot.AddCommand(new AddMemberCommand());
-  klBot.AddCommand(new DeleteAdmin());
-  klBot.AddCommand(new GetProfileInfoCommand());
-  klBot.AddCommand(new TestCommand());
-  klBot.AddCommand(new OtherCommand());
-  klBot.AddCommand(new DuelCommand());
-  klBot.AddCommand(new DuelWinCommand());
-  klBot.AddCommand(new SeeMembersCommand());
+const klBot = new Bot({ prefix: "!", coolDownSecondsTime: 1, maxQueueMsgs: 5 });
+klBot.AddCommand(new WolfCommand());
+klBot.AddCommand(new HelpCommand());
+klBot.AddCommand(new ReceiveImgCommand());
+klBot.AddCommand(new AddMemberCommand());
+klBot.AddCommand(new DeleteAdmin());
+klBot.AddCommand(new GetProfileInfoCommand());
+klBot.AddCommand(new TestCommand());
+klBot.AddCommand(new OtherCommand());
+klBot.AddCommand(new DuelCommand());
+klBot.AddCommand(new DuelWinCommand());
+klBot.AddCommand(new VerMiembrosCommand());
+klBot.AddCommand(new RegistrarGrupoCommand());
+klBot.AddCommand(new RegisteredGroupsCommand());
 
-  klBot.StartBot();
-  return klBot;
-}
-
-Main();
+klBot.StartBot();
